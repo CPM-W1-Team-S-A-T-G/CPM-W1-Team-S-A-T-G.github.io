7766dad15b29882bb71fc11f17ddd47d8ce36889
Shervine-Wong <vinevine301@gmail.com>
2025-07-07 22:13:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-
   const navLinks = document.querySelectorAll('.nav-links a');
-
   const currentPath = window.location.pathname.split('/').pop();
 
-
-
   navLinks.forEach(link => {
-
     const linkPath = link.getAttribute('href');
-
     if (linkPath === currentPath) {
-
       link.classList.add('active');
-
     }
-
   });
-
 });
